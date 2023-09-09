c23c1aef9219084e60436c9d33b3e3a36adcb05b
iamkanishka <kanishkabc123@gmail.com>
2023-09-09 06:54:55 +0530

    Arranging Folder Structure

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,10 @@
+const Authlayout = ({children}:{children: React.ReactNode}) => {
+    return ( 
+      <div className="bg-orange-500 h-full">
+        <div>{children}</div>
+        </div>
+
+     );
+}
+ 
+export default Authlayout;
