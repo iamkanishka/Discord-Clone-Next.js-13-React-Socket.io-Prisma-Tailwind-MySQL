7b4e37df3e1a5cc4e3248422aa1c0c05d5c28e54
iamkanishka <kanishkabc123@gmail.com>
2023-09-29 13:28:31 +0530

    Worked on Chat Input component

--- components/Chat/chat-messages.tsx
+++ components/Chat/chat-messages.tsx
@@ -5,6 +5,9 @@ import ChatWelcome from "@/components/Chat/chat-welcome";
 import { useChatQuery } from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment } from "react";
+import { ChatItem } from "./chat-item";
+
+import { format } from "date-fns";
 
 interface ChatMessagesProps {
   name: string;
@@ -35,6 +38,8 @@ const ChatMessages = ({
   paramValue,
   type,
 }: ChatMessagesProps) => {
+
+  const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
   const queryKey = `chat:${chatId}`
 
@@ -79,7 +84,19 @@ const ChatMessages = ({
     <Fragment key={i}>
       {group.items.map((message:MessageWithMemberWithProfile)=>(
         <div key={message.id}>
-          {message.content}
+         <ChatItem
+                key={message.id}
+                id={message.id}
+                currentMember={member}
+                member={message.member}
+                content={message.content}
+                fileUrl={message.fileUrl}
+                deleted={message.deleted}
+                timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                isUpdated={message.updatedAt !== message.createdAt}
+                socketUrl={socketUrl}
+                socketQuery={socketQuery}
+              />
         </div>
       ))}
 
